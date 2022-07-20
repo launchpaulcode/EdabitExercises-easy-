e528f6c353b8c3ae9c24015301ce68f14d7cc0fa
launchpaulcode <povilas.kot@gmail.com>
2022-07-20 12:43:23 +0300

    up

--- script.js
+++ script.js
@@ -8,7 +8,35 @@ function convertHoursToSeconds(hours)
 
  {
 
-    return hours.map(hours => hours * 60) 
+    return hours.map(hours => hours * 360) 
 
+}
+
+console.log("Create a function which returns the number of true values there are in an array.")
+
+let values = [];
+let a = 5;                          // number of boolean elements in an array
+for (var i = 0; i < a; i++) 
+{
+    values[i] = Boolean(Math.round( Math.random())) 
+}
+console.log(values);
+console.log(valueTrueReturn);
+
+function valueTrueReturn(values) 
+{
+    return values.filter(values => values == true).length
+
+}
+values = valueTrueReturn(values);
+
+if(values == 1)
+{
+    console.log( "There is " + values + " true value in the array")
+
+}
+else 
+{
+    console.log( "There are " + values + " true values in the array")
 }
 
